a88da386e9ebbd003b151454fe7c19bf934591e2
Louis <luegraham@gmail.com>
2021-04-14 22:17:32 +1000

    Polished a little more, added an index to be downloaded

--- index.js
+++ index.js
@@ -4,70 +4,81 @@
 
 
 if(!document.body.classList.contains("eraser-initiated")) {
-  if(!document.querySelector('#erase-style')){
-    var styles = `
-      .ready-for-poetry .erase-able:hover {
-        color: tomato;
-        cursor: pointer;
-      }
-      .preparing-poetry {
-        cursor: pointer !important;
-      }
-      .highlighted {
-        box-shadow: 0px 0px 1px 1px black !important;
-        box-sizing: border-box !important;
-      }
-    `;
-    var styleSheet = document.createElement("style");
-    styleSheet.type = "text/css";
-    styleSheet.innerText = styles;
-    styleSheet.id = "erase-style";
-    document.head.appendChild(styleSheet);
-  }
+	if(!document.querySelector('#erase-style')){
+  	var styles = `
+  		.ready-for-poetry .erase-able:hover {
+  		color: tomato;
+  		opacity: 0;
+  		cursor: pointer;
+  		}
+  		.preparing-poetry {
+  		cursor: pointer !important;
+  		}
+  		.highlighted {
+  		box-shadow: 0px 0px 1px 1px black !important;
+  		}
+  	`;
+  	var styleSheet = document.createElement("style");
+  	styleSheet.type = "text/css";
+  	styleSheet.innerText = styles;
+  	styleSheet.id = "erase-style";
+  	document.head.appendChild(styleSheet);
+	}
 
-  var init = (e) => {
+	var init = (el) => {
+		el.innerHTML = el.innerText.split("").map(w => {
+			return `<span class="erase-able">${w}</span>`
+		}).join("");
+		el.classList.add('ready-for-poetry');
+		el.classList.remove('highlighted');
+		el.style.transition = "background .3s ease-in-out";
+		el.style.background = "silver";
+		el.style.userSelect = "none";
+
+		el.addEventListener("click", erase);
+		el.addEventListener("mousedown", erase);
+		el.addEventListener("mouseover", erase);
+
+		document.body.classList.remove("preparing-poetry");
+		document.removeEventListener('click', init);
+
+		window.setTimeout(function(){
+		el.style.background = "";
+		}, 300);
+	};
+	var erase = (e) => {
+  	var eventRight = e.type == "mouseover" && window.mouseDown ? true
+  					: e.type == "click" ? true
+  					: e.type == "mousedown" ? true
+  					: false;
+  	if(eventRight && e.target.classList.contains('erase-able')){
+  		e.target.style.opacity = 0;
+  	}
+	};
+	var mouseDown = (e) => {window.mouseDown = true;};
+	var mouseUp = (e) => {window.mouseDown = false; };
+
+	function addHighlight(target) {
+		if(document.body.classList.contains("preparing-poetry")){
+			target.classList.add('highlighted')
+		};
+	}
+	function removeHighlight(target) {
+		if(document.body.classList.contains("preparing-poetry")){
+			target.classList.remove('highlighted')
+		};
+	}
+	document.addEventListener('mouseover',function(e) {addHighlight(e.target);});
+	document.addEventListener('mouseout',function(e) {removeHighlight(e.target);});
+
+	document.addEventListener('click', function(e){
     if(document.body.classList.contains("preparing-poetry")){
       e.preventDefault();
       e.stopPropagation();
-
-      e.target.innerHTML = e.target.innerText.split("").map(w => {return `<span class="erase-able">${w}</span>`}).join("");
-      e.target.classList.add('ready-for-poetry');
-      e.target.classList.remove('highlighted');
-      e.target.style.transition = "background .3s ease-in-out";
-      e.target.style.background = "silver";
-      e.target.style.userSelect = "none";
-
-      e.target.addEventListener("click", erase);
-      e.target.addEventListener("mousedown", erase);
-      e.target.addEventListener("mouseover", erase);
-
-      document.body.classList.remove("preparing-poetry");
-      document.removeEventListener('click', init);
-
-      window.setTimeout(function(){
-        e.target.style.background = "";
-      }, 300);
+      init(e.target);
     }
-  };
-  var erase = (e) => {
-    var eventRight = e.type == "mouseover" && window.mouseDown ? true
-                    : e.type == "click" ? true
-                    : e.type == "mousedown" ? true
-                    : false;
-    if(eventRight && e.target.classList.contains('erase-able')){
-      e.target.style.opacity = 0;
-    }
-  };
-  var mouseDown = (e) => {window.mouseDown = true;};
-  var mouseUp = (e) => {window.mouseDown = false; };
-
-  function addHighlight(target) {if(document.body.classList.contains("preparing-poetry")){target.classList.add('highlighted')};}
-  function removeHighlight(target) {if(document.body.classList.contains("preparing-poetry")){target.classList.remove('highlighted')};}
-  document.addEventListener('mouseover',function(e) {addHighlight(e.target);});
-  document.addEventListener('mouseout',function(e) {removeHighlight(e.target);});
-
-  document.addEventListener('click', init);
-  document.addEventListener('mousedown', mouseDown);
-  document.addEventListener('mouseup', mouseUp);
-  document.body.classList.add("preparing-poetry");
+  });
+	document.addEventListener('mousedown', mouseDown);
+	document.addEventListener('mouseup', mouseUp);
+	document.body.classList.add("preparing-poetry");
 }
